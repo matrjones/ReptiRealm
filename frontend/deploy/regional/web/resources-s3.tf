531d874264358c3bf41d7f8cd7ff86dbf1eb1a32
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-13 22:42:06 +0000

    fix: updated s3 perms

--- frontend/deploy/regional/web/resources-s3.tf
+++ frontend/deploy/regional/web/resources-s3.tf
@@ -1,9 +1,31 @@
-resource "aws_s3_bucket" "static_images" {
+resource "aws_s3_bucket" "this" {
   bucket = "repti-realm-images-${var.environment}"
 }
 
-resource "aws_s3_bucket_cors_configuration" "cors" {
-  bucket = aws_s3_bucket.static_images.id
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket     = aws_s3_bucket.this.id
+  acl        = "public-read"
+  depends_on = [aws_s3_bucket_public_access_block.this]
+}
+
+resource "aws_s3_bucket_cors_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
 
   cors_rule {
     allowed_headers = ["*"]
@@ -19,13 +41,8 @@ resource "aws_s3_bucket_cors_configuration" "cors" {
 
 }
 
-resource "aws_s3_bucket_acl" "acl" {
-  bucket = aws_s3_bucket.static_images.id
-  acl    = "public-read"
-}
-
-resource "aws_s3_bucket_policy" "policy" {
-  bucket = aws_s3_bucket.static_images.id
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
   policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -36,8 +53,8 @@ resource "aws_s3_bucket_policy" "policy" {
             "Principal": "*",
             "Action": "s3:GetObject",
             "Resource": [
-                "arn:aws:s3:::${aws_s3_bucket.static_images.id}",
-                "arn:aws:s3:::${aws_s3_bucket.static_images.id}/*"
+                "arn:aws:s3:::${aws_s3_bucket.this.id}",
+                "arn:aws:s3:::${aws_s3_bucket.this.id}/*"
             ]
         }
     ]
