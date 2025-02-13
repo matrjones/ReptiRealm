a0d60c7491a16ee5cf95f0201c7af79adf3df8f0
Arran4567 <Arran4567@gmail.com>
2025-02-13 18:35:21 +0000

    Added swagger

--- API/ReptiRealm/src/ReptiRealm/Startup.cs
+++ API/ReptiRealm/src/ReptiRealm/Startup.cs
@@ -1,4 +1,6 @@
-﻿namespace ReptiRealm
+﻿using Microsoft.OpenApi.Models;
+
+namespace ReptiRealm
 {
     public class Startup
     {
@@ -6,6 +8,36 @@
         {
             services.AddControllers();
             services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "ReptiRealm",
+                    Version = "v1"
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme {
+                            Reference = new OpenApiReference {
+                                Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -14,6 +46,12 @@
             app.UseStaticFiles();
             app.UseRouting();
             app.UseAuthorization();
+
+            if(env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.UseEndpoints(endpoints =>
             {
