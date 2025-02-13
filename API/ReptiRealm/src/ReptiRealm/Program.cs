9910f2e48892b3baca053293a7a50b2a49711250
Arran4567 <Arran4567@gmail.com>
2025-02-13 18:20:43 +0000

    Restructured Program file

--- API/ReptiRealm/src/ReptiRealm/Program.cs
+++ API/ReptiRealm/src/ReptiRealm/Program.cs
@@ -1,19 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Hosting;
 
-// Add services to the container.
-builder.Services.AddControllers();
+namespace ReptiRealm
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-// Add AWS Lambda support. When application is run in Lambda Kestrel is swapped out as the web server with Amazon.Lambda.AspNetCoreServer. This
-// package will act as the webserver translating request and responses between the Lambda event source and ASP.NET Core.
-builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
-
-var app = builder.Build();
-
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-
-app.MapGet("/", () => "Welcome to running ASP.NET Core Minimal API on AWS Lambda");
-
-app.Run();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ API/ReptiRealm/src/ReptiRealm/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace ReptiRealm
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Welcome to running ASP.NET Core Minimal API on AWS Lambda");
+                });
+            });
+        }
+    }
+}
