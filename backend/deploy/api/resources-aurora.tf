f1682d2df91ee6bd6c0d62cb3a2ce5fd3fbb9fbd
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-14 23:38:19 +0000

    fix: region

--- backend/deploy/api/resources-aurora.tf
+++ backend/deploy/api/resources-aurora.tf
@@ -2,7 +2,7 @@ resource "aws_rds_cluster" "default" {
   cluster_identifier      = "repti-realm-${var.environment_name}"
   engine                  = "aurora-mysql"
   engine_version          = "8.0.mysql_aurora.3.04.0"
-  availability_zones      = ["us-west-1"]
+  availability_zones      = ["us-east-1"]
   database_name           = "repti-realm-${var.environment_name}"
   master_username         = "repti-realm"
   master_password         = "okndwuhgf093"
