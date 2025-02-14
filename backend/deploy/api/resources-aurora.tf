91e385d7e429519bca5a1f99010e247e06464b51
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-14 23:32:36 +0000

    feat: mysql database, some nav bar stuff

---
+++ backend/deploy/api/resources-aurora.tf
@@ -0,0 +1,11 @@
+resource "aws_rds_cluster" "default" {
+  cluster_identifier      = "repti-realm-${var.environment_name}"
+  engine                  = "aurora-mysql"
+  engine_version          = "5.7.mysql_aurora.2.03.2"
+  availability_zones      = ["us-west-1"]
+  database_name           = "repti-realm-${var.environment_name}"
+  master_username         = "repti-realm"
+  master_password         = "okndwuhgf093"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+}
--- frontend/source/app/(dashboard)/dashboard/page.tsx
+++ frontend/source/app/(dashboard)/dashboard/page.tsx
@@ -2,10 +2,9 @@
 
 export default function Home() {
   return (
-    <div className="w-full h-full bg-slate-100">
+    <div className="w-full h-full bg-slate-100 ml-64 ">
       <main className="w-full h-full">
         <div className="w-[500px] h-[500px] bg-black"></div>
-
       </main>
     </div>
   );
