e12db9bfaf7dd06fad8ffa49cbfb09f75487b51a
Anthony Key <anthony@aruvr.com>
2025-02-14 16:09:20 +0000

    chore: removed unused code--no-deploy

--- frontend/source/app/(dashboard)/dashboard/page.tsx
+++ frontend/source/app/(dashboard)/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="w-full h-full bg-slate-900">
+    <div className="w-full h-full bg-slate-100">
       <main className="w-full h-full"></main>
     </div>
   );
