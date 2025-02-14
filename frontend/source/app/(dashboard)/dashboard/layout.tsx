56e37689874af0b6dbb558646bd406c45fea76dd
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-14 15:14:43 +0000

    chore: removed junk for boiler plate project

---
+++ frontend/source/app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/globals.css";
+import Navbar from "@/components/global/dashboard/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "ReptRealm",
+  description: "A reptile tracking app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ frontend/source/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="w-full h-full bg-slate-900">
+      <main className="w-full h-full"></main>
+    </div>
+  );
+}
