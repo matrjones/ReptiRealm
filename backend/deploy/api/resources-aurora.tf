b48ef3d915efedccd2832fa5a39d3f5eb7c88e06
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-14 23:35:49 +0000

    fix: version bump

--- backend/deploy/api/resources-aurora.tf
+++ backend/deploy/api/resources-aurora.tf
@@ -1,7 +1,7 @@
 resource "aws_rds_cluster" "default" {
   cluster_identifier      = "repti-realm-${var.environment_name}"
   engine                  = "aurora-mysql"
-  engine_version          = "5.7.mysql_aurora.2.03.2"
+  engine_version          = "8.0.mysql_aurora.3.04.0"
   availability_zones      = ["us-west-1"]
   database_name           = "repti-realm-${var.environment_name}"
   master_username         = "repti-realm"
