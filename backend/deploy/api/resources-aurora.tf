a8ee6f31974b81e45ac0f5365f972dd8e9cd4207
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-14 23:40:08 +0000

    fix: terraform default values

--- backend/deploy/api/resources-aurora.tf
+++ backend/deploy/api/resources-aurora.tf
@@ -2,7 +2,7 @@ resource "aws_rds_cluster" "default" {
   cluster_identifier      = "repti-realm-${var.environment_name}"
   engine                  = "aurora-mysql"
   engine_version          = "8.0.mysql_aurora.3.04.0"
-  availability_zones      = ["us-east-1"]
+  availability_zones      = ["us-west-2a", "us-west-2b", "us-west-2c"]
   database_name           = "repti-realm-${var.environment_name}"
   master_username         = "repti-realm"
   master_password         = "okndwuhgf093"
