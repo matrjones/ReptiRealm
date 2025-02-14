29404122c0068ff107c80f9a3849ba5a7f695e26
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-14 23:48:01 +0000

    fix: no more dashes!!!

--- backend/deploy/api/resources-aurora.tf
+++ backend/deploy/api/resources-aurora.tf
@@ -1,9 +1,9 @@
 resource "aws_rds_cluster" "default" {
-  cluster_identifier      = "repti-realm-${var.environment_name}"
+  cluster_identifier      = "reptirealm${var.environment_name}"
   engine                  = "aurora-mysql"
   engine_version          = "8.0.mysql_aurora.3.04.0"
   database_name           = "reptirealm${var.environment_name}"
-  master_username         = "repti-realm"
+  master_username         = "reptirealm"
   master_password         = "okndwuhgf093"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
