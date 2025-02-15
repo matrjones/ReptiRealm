ba53d7ec9639001a5089d09f77809b093e5083eb
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-15 01:02:00 +0000

    fix: extra rds settings

--- backend/scripts/api/plan.sh
+++ backend/scripts/api/plan.sh
@@ -1,10 +1,5 @@
 . backend/scripts/api/set-vars.sh $1 $2
 
-
-terragrunt run-all refresh
-
-terragrunt state rm aws_rds_cluster.default
-
 terragrunt plan -input=false \
     -out=$PLAN_FILE_PATH \
     --terragrunt-non-interactive \
