434486e9198dd6417127c53cb59cbf380fc5caae
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-15 01:20:24 +0000

    fix: destroy

--- backend/scripts/api/destroy.sh
+++ backend/scripts/api/destroy.sh
@@ -1,6 +1,7 @@
+
 . backend/scripts/api/set-vars.sh $1 $2
 
-terragrunt plan -input=false \
-    -out=$PLAN_FILE_PATH \
+terragrunt run-all destroy \
+    -auto-approve \
     --terragrunt-non-interactive \
     --terragrunt-working-dir $TERRAGRUNT_DIRECTORY
--- backend/scripts/api/refresh.sh
+++
@@ -1,6 +0,0 @@
-. backend/scripts/api/set-vars.sh $1 $2
-
-terragrunt run-all refresh -input=false \
-    -out=$PLAN_FILE_PATH \
-    --terragrunt-non-interactive \
-    --terragrunt-working-dir $TERRAGRUNT_DIRECTORY
