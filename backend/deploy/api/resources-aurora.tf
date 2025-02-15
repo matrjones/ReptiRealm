2dec82d7e32bb6c878b6033beb38cc93c46e1024
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-15 00:18:52 +0000

    fix: added vpc

--- backend/deploy/api/resources-aurora.tf
+++ backend/deploy/api/resources-aurora.tf
@@ -1,12 +1,15 @@
 resource "aws_rds_cluster" "default" {
-  cluster_identifier      = "reptirealm${var.environment_name}"
-  engine                  = "aurora-mysql"
-  engine_version          = "8.0.mysql_aurora.3.04.0"
-  database_name           = "reptirealm${var.environment_name}"
-  master_username         = "reptirealm"
-  master_password         = "okndwuhgf093"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
+  cluster_identifier = "reptirealm${var.environment_name}"
+  engine             = "aurora-mysql"
+  engine_version     = "8.0.mysql_aurora.3.04.0"
+  database_name      = "reptirealm${var.environment_name}"
+  master_username    = "reptirealm"
+  master_password    = "okndwuhgf093"
+
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.my_db_subnet_group.name
+
+  skip_final_snapshot = true
 }
 
 output "rds_cluster_endpoint" {
@@ -17,4 +20,13 @@ resource "aws_ssm_parameter" "rds_connection_string" {
   name  = "/ReptiRealm/rds_connection_string"
   type  = "String"
   value = "Server=${aws_rds_cluster.default.endpoint};Database=reptirealm${var.environment_name};User Id=reptirealm;Password=okndwuhgf093;"
+}
+
+resource "aws_db_subnet_group" "my_db_subnet_group" {
+  name       = "reptrealm-db-subnet-group-${var.environment_name}"
+  subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+
+  tags = {
+    Name = "Subnet group for ReptiRealm RDS ${var.environment_name}"
+  }
 }
