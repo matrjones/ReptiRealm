e5de9a76220060f64b36580f51b4e451c6d15ab9
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-15 00:58:36 +0000

    fix: removed the rds cluster

--- backend/scripts/api/plan.sh
+++ backend/scripts/api/plan.sh
@@ -1,11 +1,9 @@
 . backend/scripts/api/set-vars.sh $1 $2
 
 
-terragrunt run-all refresh -input=false \
-    -out=$PLAN_FILE_PATH \
-    --terragrunt-non-interactive \
-    --terragrunt-working-dir $TERRAGRUNT_DIRECTORY
+terragrunt run-all refresh
 
+terragrunt state rm aws_rds_cluster.default
 
 terragrunt plan -input=false \
     -out=$PLAN_FILE_PATH \
