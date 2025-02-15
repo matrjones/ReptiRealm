6415cd96cc8d9482a02dae603628dd61d7bd64b9
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-15 01:49:34 +0000

    fix: removed availability zone

--- backend/deploy/api/resources-vpc.tf
+++ backend/deploy/api/resources-vpc.tf
@@ -3,15 +3,13 @@ resource "aws_vpc" "my_vpc" {
 }
 
 resource "aws_subnet" "subnet_a" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "eu-central-1a"
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
 }
 
 resource "aws_subnet" "subnet_b" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "eu-central-1b"
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
 }
 
 resource "aws_security_group" "rds_sg" {
