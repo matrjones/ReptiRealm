6d5ef92c3d62a4fb0f0de8c17c7d0ebd0b43ca99
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-15 00:55:55 +0000

    fix: refresh terragrunt

--- backend/scripts/api/plan.sh
+++ backend/scripts/api/plan.sh
@@ -1,5 +1,12 @@
 . backend/scripts/api/set-vars.sh $1 $2
 
+
+terragrunt run-all refresh -input=false \
+    -out=$PLAN_FILE_PATH \
+    --terragrunt-non-interactive \
+    --terragrunt-working-dir $TERRAGRUNT_DIRECTORY
+
+
 terragrunt plan -input=false \
     -out=$PLAN_FILE_PATH \
     --terragrunt-non-interactive \
