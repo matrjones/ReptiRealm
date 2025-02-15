7b3bf6f5f2738acefbc241ae57674263beb36011
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-15 01:07:44 +0000

    fix: might removed cluster

--- backend/scripts/api/plan.sh
+++ backend/scripts/api/plan.sh
@@ -1,5 +1,7 @@
 . backend/scripts/api/set-vars.sh $1 $2
 
+terraform taint aws_rds_cluster.default --terragrunt-working-dir $TERRAGRUNT_DIRECTORY
+
 terragrunt plan -input=false \
     -out=$PLAN_FILE_PATH \
     --terragrunt-non-interactive \
