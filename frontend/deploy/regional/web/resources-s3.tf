506211fd3326fd470f9c130772d40cce416cd7a2
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-16 12:46:21 +0000

    fix: static images name

--- frontend/deploy/regional/web/resources-s3.tf
+++ frontend/deploy/regional/web/resources-s3.tf
@@ -53,8 +53,8 @@ resource "aws_s3_bucket_policy" "this" {
             "Principal": "*",
             "Action": "s3:GetObject",
             "Resource": [
-                "arn:aws:s3:::${aws_s3_bucket.this.id}",
-                "arn:aws:s3:::${aws_s3_bucket.this.id}/*"
+                "arn:aws:s3:::${aws_s3_bucket.static_images.id}",
+                "arn:aws:s3:::${aws_s3_bucket.static_images.id}/*"
             ]
         }
     ]
