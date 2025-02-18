c31cbc9fa8e92daefc16a87f9a46f3be9f5188c4
Anthony Key <anthonykey123@hotmail.co.uk>
2025-02-18 21:59:18 +0000

    feat: loads data on dashboard

--- frontend/source/app/(dashboard)/dashboard/page.tsx
+++ frontend/source/app/(dashboard)/dashboard/page.tsx
@@ -1,26 +1,26 @@
 "use client";
+import { getReptiles } from "@/app/actions/reptile";
 import Filter from "@/components/global/Filter";
+import Spinner from "@/components/global/Spinner";
 import ImageNotFound from "@/public/placeholder-image.jpg";
-import { useState } from "react";
-
-const reptiles = [
-  { name: "Bearded Dragon", temp: "85°F", feedIn: "3 days", progress: "60%" },
-  { name: "Leopard Gecko", temp: "78°F", feedIn: "1 day", progress: "90%" },
-  { name: "Ball Python", temp: "80°F", feedIn: "5 days", progress: "40%" },
-  { name: "Crested Gecko", temp: "75°F", feedIn: "2 days", progress: "70%" },
-  { name: "Red-Eared Slider", temp: "78°F", feedIn: "4 days", progress: "50%" },
-  { name: "Green Iguana", temp: "88°F", feedIn: "6 days", progress: "30%" },
-  {
-    name: "Blue-Tongue Skink",
-    temp: "82°F",
-    feedIn: "2 days",
-    progress: "65%",
-  },
-  { name: "King Cobra", temp: "85°F", feedIn: "7 days", progress: "20%" },
-];
+import { AnimalForm } from "@/types/types";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [showFilter, setShowFilter] = useState<boolean>(false);
+  const [reptiles, setReptiles] = useState<AnimalForm[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchReptiles = async () => {
+      setIsLoading(true);
+      const response = (await getReptiles()) as AnimalForm[];
+      setReptiles(response);
+      setIsLoading(false);
+    };
+
+    fetchReptiles();
+  }, []);
 
   return (
     <div className="w-full h-full pl-64 mt-12 p-6">
@@ -47,36 +47,44 @@ export default function Home() {
           </div>
         </div>
         {showFilter && <Filter />}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {reptiles.map((reptile, index) => (
-            <div key={index} className="bg-white p-6 rounded-lg shadow-md">
-              <img
-                src={ImageNotFound.src}
-                alt={reptile.name}
-                className="w-full h-48 object-cover rounded-md"
-              />
-              <h2 className="text-xl font-semibold mt-4">{reptile.name}</h2>
-              <p className="text-gray-500 mt-1">
-                Tracking progress & feeding schedule
-              </p>
-              <div className="mt-4">
-                <p className="text-sm text-gray-600 mb-1">
-                  Next Feed In: {reptile.feedIn}
+        {isLoading ? (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 h-[400px]">
+            <div className="bg-white animate-pulse p-6 rounded-lg shadow-md items-center justify-center flex" />
+            <div className="bg-white animate-pulse p-6 rounded-lg shadow-md items-center justify-center flex" />
+            <div className="bg-white animate-pulse p-6 rounded-lg shadow-md items-center justify-center flex" />
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {reptiles.map((reptile, index) => (
+              <div key={index} className="bg-white p-6 rounded-lg shadow-md">
+                <img
+                  src={ImageNotFound.src}
+                  alt={reptile.name}
+                  className="w-full h-48 object-cover rounded-md"
+                />
+                <h2 className="text-xl font-semibold mt-4">{reptile.name}</h2>
+                <p className="text-gray-500 mt-1">
+                  Tracking progress & feeding schedule
                 </p>
-                <div className="w-full bg-gray-200 rounded-full h-4 overflow-hidden">
-                  <div
-                    className="h-full bg-green-500 transition-all duration-500"
-                    style={{ width: reptile.progress }}
-                  ></div>
+                <div className="mt-4">
+                  <p className="text-sm text-gray-600 mb-1">
+                    Next Feed In: {reptile.sex}
+                  </p>
+                  <div className="w-full bg-gray-200 rounded-full h-4 overflow-hidden">
+                    <div
+                      className="h-full bg-green-500 transition-all duration-500"
+                      style={{ width: reptile.morphs.length }}
+                    ></div>
+                  </div>
+                </div>
+                <div className="mt-4 flex justify-between text-sm text-gray-700">
+                  <span>Temperature:</span>
+                  <span className="font-medium">{reptile.morphs[0].name}</span>
                 </div>
               </div>
-              <div className="mt-4 flex justify-between text-sm text-gray-700">
-                <span>Temperature:</span>
-                <span className="font-medium">{reptile.temp}</span>
-              </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </main>
     </div>
   );
