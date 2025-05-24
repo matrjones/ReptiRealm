996965f0e3267dd2a7545ba903047b6f6a917d03
Anthony Key <tonykey132@gmail.com>
2025-05-24 23:24:02 +0100

    fix: lol missed someething

--- alexapi/scripts/api/set-vars.sh
+++ alexapi/scripts/api/set-vars.sh
@@ -7,6 +7,6 @@ export PROJECT_NAME="alex-api"
 export ENVIRONMENT_NAME=$1
 export TERRAGRUNT_DIRECTORY="alexapi/deploy/api"
 export PLAN_FILE_PATH="/tmp/alex-api.tfplan"
-export TF_VAR_lambda_package_file_path="$PWD/alexapi/ReptiRealm/publish"
+export TF_VAR_lambda_package_file_path="$PWD/alexapi/AlexAPI/publish"
 export TF_VAR_environment_name=$1
 export ENVIRONMENT_NAME=$1
