3722bcb936f1b03b4c50b97bf979ca95a9c900fb
Anthony Key <tonykey132@gmail.com>
2025-05-24 23:26:34 +0100

    fix: removed junk

--- alexapi/deploy/api/resources-certificate.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_acm_certificate" "cert" {
-  domain_name       = "api-${var.environment_name}.pineappleexplorers.com"
-  validation_method = "DNS"
-  provider          = aws.useast1
-}
