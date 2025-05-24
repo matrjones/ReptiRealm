11985defc103c10231759962f5feec4a3311f9af
Anthony Key <tonykey132@gmail.com>
2025-05-24 23:21:24 +0100

    fix: folder restructure

--- alexapi/scripts/api/build.sh
+++ alexapi/scripts/api/build.sh
@@ -1,2 +1,2 @@
-dotnet restore alexapi/ReptiRealm/src/ReptiRealm/AlexAPI.csproj
-dotnet publish alexapi/ReptiRealm/src/ReptiRealm/AlexAPI.csproj -c Release -r linux-x64 --self-contained false -o ./alexapi/ReptiRealm/publish/
+dotnet restore alexapi/AlexAPI/AlexAPI.csproj
+dotnet publish alexapi/AlexAPI/AlexAPI.csproj -c Release -r linux-x64 --self-contained false -o ./alexapi/AlexAPI/publish/
