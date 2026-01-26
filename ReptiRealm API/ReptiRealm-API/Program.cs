ae6f0307abbd254415cdb5495664f29180b24f39
matrjones <jones.mathewr@gmail.com>
2026-01-26 14:50:49 +0000

    created initial migration

--- ReptiRealm API/ReptiRealm-API/Program.cs
+++ ReptiRealm API/ReptiRealm-API/Program.cs
@@ -13,13 +13,17 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
