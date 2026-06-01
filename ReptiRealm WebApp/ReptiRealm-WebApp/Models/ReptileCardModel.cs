7d3d8e9cbd57502315b2d83af1df8cbd754d5e8d
mrjones <mjmoither@gmail.com>
2026-06-01 18:48:53 +0100

    Created PageSection and looped to show all reptiles

---
+++ ReptiRealm WebApp/ReptiRealm-WebApp/Models/ReptileCardModel.cs
@@ -0,0 +1,8 @@
+﻿using ReptiRealm_WebApp.Models.Enums;
+
+public class ReptileCardModel
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Species {  get; set; }
+    public Sex Sex { get; set; } = Sex.Unknown;
+}
