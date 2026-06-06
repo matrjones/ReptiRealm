7ec16970ef3165506027c9d54947f121dc218f3f
Arran4567 <Arran4567@gmail.com>
2026-06-06 14:15:30 +0100

    Implemented calendar, modals WIP

---
+++ ReptiRealm WebApp/ReptiRealm-WebApp/Models/DTOs/FeedEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace ReptiRealm_WebApp.Models.DTOs;
+
+public class FeedEvent : Event
+{
+    public Feed Feed { get; set; }
+
+}
---
+++ ReptiRealm WebApp/ReptiRealm-WebApp/Models/DTOs/FoodType.cs
@@ -0,0 +1,7 @@
+﻿namespace ReptiRealm_WebApp.Models.DTOs;
+
+public class FoodType
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+}
