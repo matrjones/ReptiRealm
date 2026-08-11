cbc13a645ed0f3bf701f92b9d78dbb96ce8923da
Mathew Jones <146335905+matrjones@users.noreply.github.com>
2026-08-11 23:45:08 +0100

    hooked up reptile card to actual reptile data (#22)
    * hooked up reptile card to actual reptile data
    
    * now passing through reptile model instead of each data individually
    
    * spacing fixed
    
    * arran made me do it
    
    * spacing problem resolved again
    
    ---------
    
    Co-authored-by: mrjones <mjmoither@gmail.com>

--- ReptiRealm WebApp/ReptiRealm-WebApp/Models/DTOs/Reptile.cs
+++ ReptiRealm WebApp/ReptiRealm-WebApp/Models/DTOs/Reptile.cs
@@ -8,6 +8,7 @@ namespace ReptiRealm_WebApp.Models.DTOs
         public required string Name { get; set; }
         public Sex Sex { get; set; } = Sex.Unknown;
         public DateTime? DateOfBirth { get; set; }
+        public DateTime? DateObtained { get; set; }
         public string? Notes { get; set; }
         public string UserId { get; set; } = null!;
         public Species? Species { get; set; }
