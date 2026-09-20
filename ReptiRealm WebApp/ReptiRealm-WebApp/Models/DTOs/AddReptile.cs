09022a57a7d45a137c4c85d5651255c32277d2b3
Arran4567 <Arran4567@gmail.com>
2026-09-20 12:21:38 +0100

    Added api connection to Reptile create

---
+++ ReptiRealm WebApp/ReptiRealm-WebApp/Models/DTOs/AddReptile.cs
@@ -0,0 +1,14 @@
+﻿    using ReptiRealm_WebApp.Models.Enums;
+
+    namespace ReptiRealm_WebApp.Models.DTOs
+    {
+        public record AddReptile
+        (
+            string Name,
+            Sex? Sex,
+            Guid? SpeciesId,
+            DateTime? DateOfBirth,
+            DateTime? DateObtained,
+            Guid[]? MorphIds
+        );
+    }
