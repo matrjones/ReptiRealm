47502def364dc69ae687a3f12d1d5cfa18fe230f
Arran4567 <Arran4567@gmail.com>
2026-09-21 22:18:52 +0100

    Fixed error with morph not having UserId

--- ReptiRealm API/ReptiRealm-API/Domain/Entities/Morph.cs
+++ ReptiRealm API/ReptiRealm-API/Domain/Entities/Morph.cs
@@ -13,6 +13,7 @@ namespace ReptiRealm_API.Domain.Entities
 
         #region Foreign Keys
         public Guid SpeciesId { get; set; }
+        public required string UserId { get; set; }
         #endregion
 
 
